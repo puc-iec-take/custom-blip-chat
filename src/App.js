18db495fb6b1f28264c0bb5cfa034239225078dd
Luiz Mendes <developer.luizmendes@gmail.com>
2019-10-03 10:14:35 -0300

    modificação da estrutura de pastas e arquivos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Blip} from './components/Blip'
+import Blip from './components/BlipComponent/Blip'
+import AppKey from './components/KeyComponent/AppKey';
 
 function App() {
+
+
   return (
     <> 
       <Blip/>
+      <AppKey/>
     </>
   );
+
+  
 }
 
 export default App;
