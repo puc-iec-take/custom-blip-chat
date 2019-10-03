7d9bc6e6e7202d36d2cc5f2de3c184091eb2f6bf
Luiz Mendes <developer.luizmendes@gmail.com>
2019-10-03 10:13:43 -0300

    Definição base para componente key

---
+++ src/components/KeyComponent/AppKey.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppKey = () =>{
+
+    return(
+        <>
+            <input type="text" placeholder="App Key"/>
+            <button>Enviar</button>
+        </>
+    )
+}
+
+export default AppKey
